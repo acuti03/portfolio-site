e260d7f165f7df34442e8eb16137757545c5413e
Simone Acuti <acutisimone@gmail.com>
2023-12-15 16:32:46 +0100

    Create fonts.js

---
+++ app/fonts.js
@@ -0,0 +1,32 @@
+import { 
+    DM_Sans,
+    Roboto_Mono, 
+    Inter,
+    Exo_2,
+    Nunito
+} from 'next/font/google'
+
+export const dm_sans = DM_Sans({
+	subsets: ['latin'],
+	weight: '400',
+})
+
+export const roboto_mono = Roboto_Mono({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+export const inter = Inter({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+export const exo_2 = Exo_2({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+export const nunito = Nunito({
+    subsets: ['latin'],
+    weight: '700'
+})
