f9916225ecfa6b0103bdf61d10d8cdaa3408461b
Simone Acuti <acutisimone@gmail.com>
2023-12-15 16:35:01 +0100

    Add buttons component

---
+++ components/Buttons.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { IoSunnyOutline } from "react-icons/io5"
+import '@styles/globals.css'
+
+const Buttons = () => {
+	return(
+		<div className="buttons">
+			<button><IoSunnyOutline style={{display: 'flex', fontSize: '18px'}}/></button>
+		</div>
+	)
+}
+
+export default Buttons
